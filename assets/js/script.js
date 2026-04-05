f35031a3f2189ab304182760c69fc2e157896ee7
zoyaa4126 <alethafachira@gmail.com>
2026-04-05 19:05:39 +0700

    nambah fitur mood

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,37 @@ toggle.addEventListener("click", () => {
   }
 });
 
+const buttons = document.querySelectorAll('.kategori-btn');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach( b => b.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+
+const slider = document.getElementById("rangeSlider");
+const value = document.getElementById("rangeValue");
+
+slider.oninput = function() {
+    value.textContent = this.value + "KM";
+}
+
+function toggleSearch() {
+    const searchBar = document.querySelector(".search-bar");
+    searchBar.classList.toggle("active");
+}
+
+function myFunction() {
+  const nav = document.getElementById("mytopnav");
+  const icon = document.querySelector(".menu-icon");
+
+  nav.classList.toggle("responsive");
+
+  document.body.classList.remove("filter-open");
+
+  if(nav.classList.contains("responsive")){
+    icon.textContent = "close";
+  } else {
+    icon.textContent = "menu";
+  }
+}
