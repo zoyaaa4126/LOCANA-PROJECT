95e606252d565a9b70f9d762d6ac6c154bc14d0a
zoyaa4126 <alethafachira@gmail.com>
2026-04-29 20:42:15 +0700

    update login register pake tailwind

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,32 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
 
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const errorMsg = document.getElementById("error-msg");
+  // LOGIN
+  document.getElementById("loginForm").addEventListener("submit", function(e){
+    e.preventDefault();
 
-  // contoh login sederhana
-  if (username === "admin" && password === "1234") {
-    alert("Login berhasil!");
-    window.location.href = "dashboard.html"; // pindah halaman
-  } else {
-    errorMsg.textContent = "Username atau password salah!";
-  }
-});
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const errorMsg = document.getElementById("error-msg");
 
-//btn click perpindahan login register
-const masuk = document.getElementById("masuk");
-const daftar = document.getElementById("daftar");
-
-masuk.addEventListener("click", () => {
-    masuk.classList.add("active");
-    daftar.classList.remove("active");
-
-    setTimeout(() => {
-        window.location.href = "login.html";
-    }, 200);
-});
-
-daftar.addEventListener("click", () => {
-    daftar.classList.add("active");
-    masuk.classList.remove("active");
-
-    setTimeout(() => {
-      window.location.href = "register.html";
-    }, 200);
-});
-
-//visibility
-const toggle = document.getElementById("togglePassword");
-const password = document.getElementById("password");
-
-toggle.addEventListener("click", () => {
-  if (password.type === "password") {
-    password.type = "text";
-    toggle.textContent = "visibility_off"; // ganti icon
-  } else {
-    password.type = "password";
-    toggle.textContent = "visibility"; // balik lagi
-  }
-});
-
-const links = document.querySelectorAll('.kategori-btn a');
-
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        const buttons = document.querySelectorAll('.kategori-btn');
-
-        buttons.forEach(b => b.classList.remove('active'));
-        link.closest('.kategori-btn').classList.add('active');
-    });
-});
-
-const buttons = document.querySelectorAll('.kategori-btn');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        buttons.forEach( b => b.classList.remove('active'));
-        button.classList.add('active');
-    });
-});
-
-const slider = document.getElementById("rangeSlider");
-const value = document.getElementById("rangeValue");
-
-slider.oninput = function() {
-    value.textContent = this.value + "KM";
-}
-
-function myFunction() {
-  const nav = document.getElementById("mytopnav");
-  const icon = document.querySelector(".menu-icon");
-
-  nav.classList.toggle("responsive");
-
-  if(nav.classList.contains("responsive")){
-    icon.textContent = "close";
-  } else {
-    icon.textContent = "menu";
-  }
-}
-
-function toggleFilter(){
-  document.body.classList.toggle("filter-open");
-
-  document.getElementById("mytopnav").classList.remove("responsive");
-
-  document.querySelector(".menu-icon").textContent = "menu";
-}
-
-function toggleMenu() {
-    const mobileNavbar = document.querySelector(".mobile-navbar");
-    const toggleIcon = document.getElementById("toggleMenu");
-
-    if (mobileNavbar.style.display === "flex") {
-        mobileNavbar.style.display = "none";
-        toggleIcon.innerText = "menu";
+    if(email === "admin@locana.com" && password === "1234"){
+      window.location.href = "homepage.html";
     } else {
-        mobileNavbar.style.display = "flex";
-        toggleIcon.innerText = "close";
+      errorMsg.textContent = "Username atau Password Salah!";
     }
-}
+  });
+
+  // TOGGLE PASSWORD
+  const toggle = document.getElementById("togglePassword");
+  const password = document.getElementById("password");
+
+  toggle.addEventListener("click", function () {
+    if (password.type === "password") {
+      password.type = "text";
+      toggle.textContent = "visibility_off";
+    } else {
+      password.type = "password";
+      toggle.textContent = "visibility";
+    }
+  });
+
+});
