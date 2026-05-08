2680ca883eddaf8a1f4955fb11a0b9bc0f6365a1
natasyaramadhani <natasyaramadhani07@upi.edu>
2026-05-08 09:18:30 +0700

    Login & Register

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/home', function () {
     return view('home');
 });
