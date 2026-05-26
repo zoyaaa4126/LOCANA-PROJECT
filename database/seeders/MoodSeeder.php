ce7af94d2522473c2a111f577a548e8798f46d8e
najwasputri04 <najwasputri@upi.edu>
2026-05-26 21:17:46 +0700

    update table moods uhuy

--- database/seeders/MoodSeeder.php
+++ database/seeders/MoodSeeder.php
@@ -13,14 +13,50 @@ class MoodSeeder extends Seeder
     public function run(): void
     {
         $moods = [
-            ['id' => 1, 'nama' => 'Chill', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 2, 'nama' => 'Fancy', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 3, 'nama' => 'Keluarga', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 4, 'nama' => 'Romantis', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 5, 'nama' => 'Petualangan', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 6, 'nama' => 'Produktif', 'created_at' => now(), 'updated_at' => now()],
+            [
+                'id' => 1,
+                'nama' => 'Chill',
+                'deskripsi' => 'Suasana santai untuk melepas penat dan menikmati waktu luang.',
+                'icons' => 'Hot beverage.png',
+
+            ],
+            [
+                'id' => 2,
+                'nama' => 'Fancy',
+                'deskripsi' => 'Pengalaman elegan dan berkelas untuk momen spesialmu.',
+                'icons' => 'Teacup without handle.png',
+
+            ],
+            [
+                'id' => 3,
+                'nama' => 'Keluarga',
+                'deskripsi' => 'Momen hangat dan seru yang dirancang khusus kebersamaan keluarga.',
+                'icons' => 'Ferris Wheel.png',
+
+            ],
+            [
+                'id' => 4,
+                'nama' => 'Romantis',
+                'deskripsi' => 'Suasana penuh cinta dan kehangatan bersama orang tersayang.',
+                'icons' => 'red heart.png',
+
+            ],
+            [
+                'id' => 5,
+                'nama' => 'Petualangan',
+                'deskripsi' => 'Eksplorasi seru di bawah langit malam dan tantangan baru.',
+                'icons' => 'moon.png',
+
+            ],
+            [
+                'id' => 6,
+                'nama' => 'Produktif',
+                'deskripsi' => 'Fokus maksimal untuk menyelesaikan tugas dan pekerjaanmu.',
+                'icons' => 'laptop.png',
+
+            ],
         ];
-        
+
         DB::table('moods')->insert($moods);
     }
 }
