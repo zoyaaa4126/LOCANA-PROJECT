12f68143627552ff3c42ab9b72324bbd61fc5459
natasyaramadhani <natasyaramadhani07@upi.edu>
2026-06-08 09:05:58 +0700

    bismillah ini terakhir ya allah

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\kategoris;
-use App\Models\places;
+use App\Models\Places;
 use App\Models\Moods;
 
 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,6 @@
         <!-- Tailwind -->
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
-    @stack('scripts')
     <body class="bg-[#FAFAFA] text-[#333]">
 
     <header class="bg-white border-b border-gray-300 sticky top-0 z-50 shadow-sm">
@@ -134,10 +133,8 @@
     </div>
 
     @include('components.modal-logout')
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @stack('scripts')
-
-    @push('scripts')
+    <script src="{{ asset('js/script.js') }}"></script>
+    
     <script>
     document.addEventListener("DOMContentLoaded", function () {
 
@@ -207,6 +204,8 @@
 
     });
     </script>
-    @endpush
+
+    @stack('scripts')
+
     </body>
 </html>
